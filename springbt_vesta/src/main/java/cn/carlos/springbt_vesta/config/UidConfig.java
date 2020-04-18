59559a14ebf99a23c815dc7f96c43a8d186b5051
carlos <17620927312@163.com>
2020-04-18 18:01:39 +0800

    添加《Spring Boot 工程集成全局唯一ID生成器 Vesta》

---
+++ springbt_vesta/src/main/java/cn/carlos/springbt_vesta/config/UidConfig.java
@@ -0,0 +1,10 @@
+package cn.carlos.springbt_vesta.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource( locations = { "classpath:ext/vesta/vesta-rest-main.xml" } )
+public class UidConfig {
+
+}
